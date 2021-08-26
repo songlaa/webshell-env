bb821035da35242e669e17756da63ba65645c71d
Daniel S <sybnex@gmail.com>
2021-08-26 19:02:43 +0200

    fixed deployment for rancher

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ fi
 
 export ORG="acend"
 export APP="theia"
-export STUDENTS=8
+export STUDENTS=4
+export PREFIX="student"
+export DOMAIN="labapp.acend.ch"
 export AUTHFILE="/home/$USER/acend-training-authfile"
 
 build() {
@@ -23,26 +25,39 @@ build() {
 }
 
 deploy() {
-    touch $AUTHFILE
+    read -p "Enter Ingress (default: nginx): " INGRESS
+    if [ -z "$INGRESS" ]; then
+        export INGRESS="nginx"
+    fi
+
+    read -p "Creating with prefix '$PREFIX' and domain '$DOMAIN' on ingress '$INGRESS'. Type yes: " yn
+    case $yn in
+        [Yy]* ) echo;;
+        * ) echo "=> answer yes to go on the next time"; exit;;
+    esac 
+
+    echo "$(date): latest credentials links" >> $AUTHFILE
     for i in $(seq 1 $STUDENTS); do
-        export STUDENT=student$i
+        export STUDENT=$PREFIX$i
         cat workspace.yaml | envsubst | kubectl apply -f -
 
 	if [[ ! "$(kubectl -n $STUDENT get secrets -o name)" =~ "basic-auth" ]]; then
 	    PW=$(date | md5sum | awk '{print $1}')
-            echo $PW | htpasswd -i -c auth student$i
+            echo $PW | htpasswd -i -c auth $PREFIX$i
 	    kubectl -n $STUDENT create secret generic basic-auth --from-file=auth
-	    echo "https://student$i:$PW@student$i.theia.acend.ch" >> $AUTHFILE
+	    echo "https://$PREFIX$i:$PW@$PREFIX$i.$DOMAIN" >> $AUTHFILE
 	    rm auth
 	    sleep 5
 	fi
+	echo
     done
+    echo -e "=> check the credentials file at: $AUTHFILE \n"
 }
 
 destroy() {
     # keeps ingresses and certs as there are expensive
     for i in $(seq 1 $STUDENTS); do
-        export STUDENT=student$i
+        export STUDENT=$PREFIX$i
         kubectl -n $STUDENT delete deploy $STUDENT-theia-deploy
         kubectl -n $STUDENT delete svc    $STUDENT-theia-svc
     done
@@ -50,7 +65,7 @@ destroy() {
 
 destroyall() {
     for i in $(seq 1 $STUDENTS); do
-        export STUDENT=student$i
+        export STUDENT=$PREFIX$i
         kubectl delete ns $STUDENT
     done
     rm $AUTHFILE
